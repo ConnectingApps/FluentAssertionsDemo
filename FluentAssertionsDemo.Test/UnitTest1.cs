63916dc8003c06454309b9f397f74378b6c0baab
Daan Acohen <daan.acohen@connectingapps.net>
2023-08-14 10:19:03 +0200

    Added one regular test

---
+++ FluentAssertionsDemo.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using FluentAssertions;
+using FluentAssertionsDemo.ToAssert;
+
+namespace FluentAssertionsDemo.Test
+{
+    public class HealthDescriptionTest
+    {
+        [Theory]
+        [InlineData(70, 180, 1.80, 21.60)]
+        public void RegularTest(decimal weightInKg, int lengthInCm, decimal expectedLengthInM, decimal expectedBmi)
+        {
+            var instance = new HealthDescription(weightInKg, lengthInCm);
+            instance.LengthInM.Should().Be(expectedLengthInM);
+            instance.Bmi.Should().Be(expectedBmi);
+        }
+    }
+}
