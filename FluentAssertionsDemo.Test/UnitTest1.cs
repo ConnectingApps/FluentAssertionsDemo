2c16c2a8934f1a6296061b9f4cfe1b2a9c1406c6
Daan Acohen <daan.acohen@connectingapps.net>
2023-08-14 10:23:50 +0200

    Added a ValueTuple unit test

--- FluentAssertionsDemo.Test/UnitTest1.cs
+++ FluentAssertionsDemo.Test/UnitTest1.cs
@@ -13,5 +13,13 @@ namespace FluentAssertionsDemo.Test
             instance.LengthInM.Should().Be(expectedLengthInM);
             instance.Bmi.Should().Be(expectedBmi);
         }
+
+        [Theory]
+        [InlineData(70, 180, 1.80, 21.60)]
+        public void ValueTupleTest(decimal weightInKg, int lengthInCm, decimal expectedLengthInM, decimal expectedBmi)
+        {
+            var instance = new HealthDescription(weightInKg, lengthInCm);
+            (instance.LengthInM, instance.Bmi).Should().Be((expectedLengthInM, expectedBmi));
+        }
     }
 }
